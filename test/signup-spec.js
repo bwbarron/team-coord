89b3c16c9344a4c8215b3c7cd4245496a610ef8b
Annie Lace <annie.c.lace@gmail.com>
2015-11-13 12:48:46 -0800

    overall HTML resolves #7

---
+++ test/signup-spec.js
@@ -0,0 +1,9 @@
+describe('sign up app', function() {
+    beforeEach(function() {
+        browser.get('http://localhost:8000');
+    });
+
+    it('must have the proper page title', function() {
+        expect(browser.getTitle()).toEqual('Sign Up Form');
+    });
+})
