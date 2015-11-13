10272e7cfaa475d2ab03ce60b22afed0d01189c3
bradholland84 <bholland.mail@gmail.com>
2015-11-13 15:41:26 -0800

    added tests

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -1,4 +1,7 @@
 describe('sign up app', function() {
+    var emailRequiredMessage = $('.email-required-error');
+    var emailInvalidMessage = $('.email-invalid-error');
+    var emailInput = element(by.model('user.email'));
     beforeEach(function() {
         browser.get('http://localhost:8000');
     });
@@ -6,4 +9,26 @@ describe('sign up app', function() {
     it('must have the proper page title', function() {
         expect(browser.getTitle()).toEqual('Sign Up Form');
     });
-})
+
+    it('must show required validation error', function() {
+        expect(emailRequiredMessage.isPresent()).toEqual(false);
+        emailInput.sendKeys('abc');
+        emailInput.clear();
+        expect(emailRequiredMessage.isPresent()).toEqual(true);
+        emailInput.sendKeys('abc');
+        expect(emailRequiredMessage.isPresent()).toEqual(false);
+    });
+
+    it('must show invalid email error', function() {
+        expect(emailInvalidMessage.isPresent()).toEqual(false);
+        emailInput.sendKeys('johnsmith123gmailcom');
+        expect(emailInvalidMessage.isPresent()).toEqual(true);
+        emailInput.clear();
+        emailInput.sendKeys('johnsmith123gmail.com');
+        expect(emailInvalidMessage.isPresent()).toEqual(true);
+        emailInput.clear();
+        emailInput.sendKeys('johnsmith123@gmail.com');
+        expect(emailInvalidMessage.isPresent()).toEqual(false);
+        emailInput.clear();
+    })
+});
