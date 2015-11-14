04c56f88f796f8eec109edbe1b7dbfc419a2e6e7
Jessie Peterson <jessie23@uw.edu>
2015-11-13 16:15:43 -0800

    added name fields

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -6,4 +6,16 @@ describe('sign up app', function() {
     it('must have the proper page title', function() {
         expect(browser.getTitle()).toEqual('Sign Up Form');
     });
-})
+
+    it('must show last name validation error', function() {
+        var requiredMsg = $('.validation-error');
+        var name = element.(by.model('user.lname'));
+        expect(requiredMsg.isPresent()).toEqual(false);
+        name.sendKeys('Johnson');
+        name.clear();
+        expect(requiredMsg.isPresent()).toEqual(true);
+        name.sendKeys('Johnson');
+        expect(requiredMsg.isPresent()).toEqual(false);
+
+    });
+});
